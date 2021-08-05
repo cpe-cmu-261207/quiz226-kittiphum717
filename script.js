ce0652e6437c556a325fd54dfc58ffda219d9697
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2021-08-05 14:13:07 +0700

    เสร็จ

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
-
+count_click = 0
 btn_toggle.onclick = () => {
-  // your code here
+  calculation.value = 630610717 - length.value
+  if(count_click === 0){
+    author.innerHTML = calculation.value
+    btn_toggle.innerHTML = "Show Author"
+    count_click = 1
+  }
+  else{
+    author.innerHTML = "630610717 KITTIPHUM SANIT"
+    btn_toggle.innerHTML = "Show Calculation"
+    count_click = 0
+  }
 }
 
 // more codes for Search and Reset buttons here
+
